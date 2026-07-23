3c0592de05faa734510054848d909177cb5c1a0a
victor <victorgabrielbarbosa88@gmail.com>
2026-07-22 21:41:12 -0300

    Corrige importações de modelos e ajusta consultas no arquivo main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, status, Depends, HTTPException, Response
-from schemas import Produto, Cupom, PromocaoSteam, ProdutoResponse, CupomResponse, PromocaoSteamResponse
-import models
-from database import engine, get_db
+from db.schemas import Produto, Cupom, PromocaoSteam, ProdutoResponse, CupomResponse, PromocaoSteamResponse
+import db.models
+from db.database import engine, get_db
 from sqlalchemy.orm import Session
 from typing import List
 
-models.Base.metadata.create_all(bind=engine)
+db.models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -22,7 +22,7 @@ app.add_middleware(
  
 @app.post("/produtos", status_code=status.HTTP_201_CREATED)
 async def criar_produto(produto: Produto, db: Session = Depends(get_db)):
-  novo_produto = models.produto(**produto.model_dump())
+  novo_produto = db.models.produto(**produto.model_dump())
   db.add(novo_produto)
   db.commit()
   db.refresh(novo_produto)
@@ -31,7 +31,7 @@ async def criar_produto(produto: Produto, db: Session = Depends(get_db)):
 
 @app.post("/cupons", status_code=status.HTTP_201_CREATED)
 async def criar_cupom(cupom: Cupom, db: Session = Depends(get_db)):
-  novo_cupom = models.cupom(**cupom.model_dump())
+  novo_cupom = db.models.cupom(**cupom.model_dump())
   db.add(novo_cupom)
   db.commit()
   db.refresh(novo_cupom)
@@ -40,15 +40,15 @@ async def criar_cupom(cupom: Cupom, db: Session = Depends(get_db)):
 
 @app.get("/produtos", response_model=List[ProdutoResponse], status_code=status.HTTP_200_OK)
 async def buscar_produtos(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
-  return db.query(models.produto).offset(skip).limit(limit).all()
+  return db.query(db.models.produto).offset(skip).limit(limit).all()
 
 @app.get("/cupons", response_model=List[CupomResponse], status_code=status.HTTP_200_OK)
 async def buscar_cupons(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
-  return db.query(models.cupom).offset(skip).limit(limit).all()
+  return db.query(db.models.cupom).offset(skip).limit(limit).all()
 
 @app.put("/produtos/{produto_id}", response_model=ProdutoResponse, status_code=status.HTTP_200_OK)
 async def atualizar_produto(produto_id: int, produto: Produto, db: Session = Depends(get_db)):
-  produto_query = db.query(models.produto).filter(models.produto.id == produto_id)
+  produto_query = db.query(db.models.produto).filter(db.models.produto.id == produto_id)
   produto_existente = produto_query.first()
   if produto_existente is None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto não encontrado")
@@ -58,7 +58,7 @@ async def atualizar_produto(produto_id: int, produto: Produto, db: Session = Dep
 
 @app.put("/cupons/{cupom_id}", response_model=CupomResponse, status_code=status.HTTP_200_OK)
 async def atualizar_cupom(cupom_id: int, cupom: Cupom, db: Session = Depends(get_db)):
-  cupom_query = db.query(models.cupom).filter(models.cupom.id == cupom_id)
+  cupom_query = db.query(db.models.cupom).filter(db.models.cupom.id == cupom_id)
   cupom_existente = cupom_query.first()
   if cupom_existente is None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cupom não encontrado")
@@ -68,7 +68,7 @@ async def atualizar_cupom(cupom_id: int, cupom: Cupom, db: Session = Depends(get
   
 @app.delete("/produtos/{produto_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def remover_produto(produto_id: int, db: Session = Depends(get_db)):
-  produto_query = db.query(models.produto).filter(models.produto.id == produto_id)
+  produto_query = db.query(db.models.produto).filter(db.models.produto.id == produto_id)
   if produto_query.first() is None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto não encontrado")
   produto_query.delete(synchronize_session=False)
@@ -77,7 +77,7 @@ async def remover_produto(produto_id: int, db: Session = Depends(get_db)):
 
 @app.delete("/cupons/{cupom_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def remover_cupom(cupom_id: int, db: Session = Depends(get_db)):
-  cupom_query = db.query(models.cupom).filter(models.cupom.id == cupom_id)
+  cupom_query = db.query(db.models.cupom).filter(db.models.cupom.id == cupom_id)
   if cupom_query.first() is None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cupom não encontrado")
   cupom_query.delete(synchronize_session=False)
@@ -86,7 +86,7 @@ async def remover_cupom(cupom_id: int, db: Session = Depends(get_db)):
 
 @app.post("/promocoes", status_code=status.HTTP_201_CREATED)
 async def criar_promocao(promocao: PromocaoSteam, db: Session = Depends(get_db)):
-  nova_promocao = models.promocao_steam(**promocao.model_dump())
+  nova_promocao = db.models.promocao_steam(**promocao.model_dump())
   db.add(nova_promocao)
   db.commit()
   db.refresh(nova_promocao)
@@ -95,11 +95,11 @@ async def criar_promocao(promocao: PromocaoSteam, db: Session = Depends(get_db))
 
 @app.get("/promocoes", response_model=List[PromocaoSteamResponse], status_code=status.HTTP_200_OK)
 async def buscar_promocoes(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
-  return db.query(models.promocao_steam).offset(skip).limit(limit).all()
+  return db.query(db.models.promocao_steam).offset(skip).limit(limit).all()
 
 @app.put("/promocoes/{promocao_id}", response_model=PromocaoSteamResponse, status_code=status.HTTP_200_OK)
 async def atualizar_promocao(promocao_id: int, promocao: PromocaoSteam, db: Session = Depends(get_db)):
-  promocao_query = db.query(models.promocao_steam).filter(models.promocao_steam.id == promocao_id)
+  promocao_query = db.query(db.models.promocao_steam).filter(db.models.promocao_steam.id == promocao_id)
   promocao_existente = promocao_query.first()
   if promocao_existente is None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Promoção não encontrada")      
@@ -109,7 +109,7 @@ async def atualizar_promocao(promocao_id: int, promocao: PromocaoSteam, db: Sess
 
 @app.delete("/promocoes/{promocao_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def remover_promocao(promocao_id: int, db: Session = Depends(get_db)):
-  promocao_query = db.query(models.promocao_steam).filter(models.promocao_steam.id == promocao_id)
+  promocao_query = db.query(db.models.promocao_steam).filter(db.models.promocao_steam.id == promocao_id)
   
   if promocao_query.first() is None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Promoção não encontrada")
